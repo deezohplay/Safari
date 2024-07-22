80782f4ec6578599c5d80d8af00cb4afd188a0b7
Rubangakene Inno Joseph <57228148+deezohplay@users.noreply.github.com>
2024-07-22 20:51:24 +0300

    created script for vehicle control

---
+++ Assets/Scripts/VehicleController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleController : MonoBehaviour
+{
+    /*
+    [Header("wheels collider")]
+    public Coliider frontLeftWheelCollider;
+    public Coliider frontRightWheelCollider;
+    public Collider backLeftWheelCollider;
+    public Collider backRightWheelCollider;
+
+    [Header("wheels tramsform")] 
+    public Transform frontLeftWheelTransform;
+    public Transform frontRighttWheelTransform;
+    public Transform backLeftWheelTransform;
+    public Transform backRightWheelTransform;
+    */
+}
